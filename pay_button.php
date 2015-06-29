d93e82d47e8773b07b3b6c767c1ae0f62d77b288
easynowbaby <david.cerny.mail@gmail.com>
2015-06-29 20:17:48 +0200

    initial commit v 0.1

---
+++ pay_button.php
@@ -0,0 +1,4 @@
+<form action="<?= $url ?>" method="post" id="gopay-payment-button">
+	<button name="pay" type="submit">Zaplatit</button>
+	<script type="text/javascript" src="https://testgw.gopay.cz/gp-gw/js/embed.js"></script>
+</form>
